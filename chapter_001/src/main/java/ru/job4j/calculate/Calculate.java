d3fac77fdb9f9b2bcc69cf3f1380710481c82da1
Averianov Yuriy <averianovj@gmail.com>
2018-07-08 22:52:26 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculate;
 
+/**
+* Class Класс для вывода строки "Hellow World"
+* @author Averianov Yuriy
+* @since 08.07.2018
+* @version 1
+*/
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Averianov Yuriy (mailto:averianovj@gmail.com)
+* @version 1
+* @since 08.07.2018
+*/
+
+package ru.job4j.calculate;
