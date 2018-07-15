4db4e8d0e569251803f112b4f4e2cfedb24c1c95
yurius <averianovj@gmail.com>
2018-07-15 21:51:34 +0300

    text

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,17 +8,23 @@ package ru.job4j.calculate;
 */
 
 public class Calculate {
+	/**
+	 * Main
+	 * @param args - args
+	 */
+
 	public static void main(String[] args) {
 		System.out.println("Hellow World");
 	}
 
-/**
-* Method echo.
-* @param name Your name
-* @return Echo plus your name
-*/
+	/**
+	* Method echo.
+	* @param name Your name
+	* @return Echo plus your name
+	*/
 
 	public String echo(String name) {
+
 		return "Echo, echo, echo:" + name;
 	}
 }
