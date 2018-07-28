f8762c76d5423f030999af03a47ea980d7846e31
Averianov Yuriy <averianovj@gmail.com>
2018-07-28 22:18:23 +0300

    6.3. Массив заполнен true или false.(реш)

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param prefix префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] value = prefix.toCharArray();
+        // проверить. что массив data имеет первые элементы одинаковые с value
+        for(int i = 0; i < data.length; i++) {
+
+
+        }
+        return result;
+    }
+}
--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,9 +2,9 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
-        for (int i = 0; i < data.length; i++) {
-           if(data[i] != false) result = true;
+        boolean result = true;
+        for (int i = 1; i < data.length; i++) {
+           if(data[0] != data[i]) result = false;
         }
         return result;
     }
