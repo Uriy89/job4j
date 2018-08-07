c11ea1b05efe72251c1eac71d6d0db3e4b6b0875
yurius <averianovj@gmail.com>
2018-08-07 17:16:14 +0300

    Обьединить два отсортированныч обьекта в один.

---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayIncludeTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayIncludeTest {
+    @Test
+    public void whenOneArrayAndArrayTwoIncludeArrayThree() {
+        int[] a = {1, 3, 5, 7, 10};
+        int[] b = {2, 4, 6, 8, 9};
+        int[] except = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        ArrayInclude summ = new ArrayInclude();
+        int[] result = summ.concat(a, b);
+        assertThat(result, is(except));
+    }
+}
